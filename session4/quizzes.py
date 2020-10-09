7e3c03321a88a47179c0110dcf7409187b32aa43
Phạm Đức <minhduc.096.99@gmail.com>
2020-10-09 21:53:01 +0700

    .

--- session4/quizzes.py
+++ session4/quizzes.py
@@ -1,10 +1,43 @@
-quiz = {
-    'question': 'con chó có mấy chân?',
-    'choices': [
-        1, 
-        4, 
-        3, 
-        2,
-    ],
-    'rightChoice': 1
-}
+quizzes = [ # collection 
+    {
+        'question': 'con chó có mấy chân?',
+        'choices': [
+            '1 chân', 
+            '2 chân', 
+            '9 chân', 
+            '4 chân',
+        ],
+        'rightChoice': 3
+    },
+    {
+        'question': 'con thỏ có mấy chân?',
+        'choices': [
+            '1 chân',
+            '2 chân',
+            '9 chân',
+            '4 chân',
+        ],
+        'rightChoice': 1
+    },
+    {
+        'question': 'con vịt có mấy chân?',
+        'choices': [
+            '1 chân',
+            '3 chân',
+            '9 chân',
+            '2 chân',
+        ],
+        'rightChoice': 1
+    }
+]
+for quiz in quizzes:
+    print(quiz['question'])
+    choices = quiz['choices']
+    for i in range(len(choices)):
+        print(f'{i+1}. {choices[i]}')
+
+    user_choice = int(input('Enter your choice ')) - 1
+    if user_choice == quiz['rightChoice']:
+        print('hoan hô!')
+    else:
+        print('buồn quá')
