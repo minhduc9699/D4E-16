5021830b63e01b2d7361bf2daf21b989d4d79f26
Phạm Đức <minhduc.096.99@gmail.com>
2020-10-23 22:04:48 +0700

    .

--- session7/mysql_intro.py
+++ session7/mysql_intro.py
@@ -9,25 +9,38 @@ client = pymysql.connect(
 cursor = client.cursor()
 
 # cursor.execute('CREATE DATABASE d4e16')
-cursor.execute('''
-    CREATE TABLE d4e16.movie(
-        id varchar(225) PRIMARY KEY,
-        title varchar(255),
-        year varchar(255),
-        writer varchar(255)
-    )
+# cursor.execute('''
+#     CREATE TABLE d4e16.movie(
+#         id varchar(225) PRIMARY KEY,
+#         title varchar(255),
+#         year varchar(255),
+#         writer varchar(255)
+#     )
+# ''')
+# cursor.execute('''
+#     CREATE TABLE d4e16.actor(
+#         name varchar(255) PRIMARY KEY
+#     )
+# ''')
+# cursor.execute(''' 
+#     CREATE TABLE d4e16.`movie-actor`(
+#         movie_id varchar(255),
+#         actor_name varchar(255),
+#         PRIMARY KEY(movie_id, actor_name),
+#         FOREIGN KEY(movie_id) REFERENCES d4e16.movie(id),
+#         FOREIGN KEY(actor_name) REFERENCES d4e16.actor(name)
+#     )
+# ''')
+
+movie_id = '1211'
+movie_name = 'tenet'
+movie_year = '2020'
+writer = 'CN'
+movie_actor = []
+
+cursor.execute(f''' 
+    INSERT INTO d4e16.movie(id, title, year, writer)
+    VALUES ('{movie_id}', '{movie_name}', '{movie_year}', '{writer}')
 ''')
-cursor.execute('''
-    CREATE TABLE d4e16.actor(
-        name varchar(255) PRIMARY KEY
-    )
-''')
-cursor.execute(''' 
-    CREATE TABLE d4e16.`movie-actor`(
-        movie_id varchar(255),
-        actor_name varchar(255),
-        PRIMARY KEY(movie_id, actor_name),
-        FOREIGN KEY(movie_id) REFERENCES d4e16.movie(id),
-        FOREIGN KEY(actor_name) REFERENCES d4e16.actor(name)
-    )
-''')
+
+client.commit()
