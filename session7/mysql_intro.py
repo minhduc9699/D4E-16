0026115d3d92d4edbfc1b42de4c668aa4f43869e
Phạm Đức <minhduc.096.99@gmail.com>
2020-10-23 21:48:27 +0700

    .

---
+++ session7/mysql_intro.py
@@ -0,0 +1,33 @@
+import pymysql
+
+client = pymysql.connect(
+    host='127.0.0.1',
+    user='root',
+    password='@gmail.com'
+)
+
+cursor = client.cursor()
+
+# cursor.execute('CREATE DATABASE d4e16')
+cursor.execute('''
+    CREATE TABLE d4e16.movie(
+        id varchar(225) PRIMARY KEY,
+        title varchar(255),
+        year varchar(255),
+        writer varchar(255)
+    )
+''')
+cursor.execute('''
+    CREATE TABLE d4e16.actor(
+        name varchar(255) PRIMARY KEY
+    )
+''')
+cursor.execute(''' 
+    CREATE TABLE d4e16.`movie-actor`(
+        movie_id varchar(255),
+        actor_name varchar(255),
+        PRIMARY KEY(movie_id, actor_name),
+        FOREIGN KEY(movie_id) REFERENCES d4e16.movie(id),
+        FOREIGN KEY(actor_name) REFERENCES d4e16.actor(name)
+    )
+''')
